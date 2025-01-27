84b1c06cd8d42a54a843896d23d9d47883b5f8a4
Markus Hakkarainen <markus.hakkarainen@student.careeria.fi>
2025-01-27 14:47:35 +0200

    Lisäystoiminto

--- NorthwindRestApi/Controllers/CustomersController.cs
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NorthwindRestApi.Models;
+using System.Linq.Expressions;
 
 namespace NorthwindRestApi.Controllers
 {
@@ -13,8 +14,53 @@ namespace NorthwindRestApi.Controllers
         [HttpGet]
         public ActionResult GetAllCustomers()
         {
-            var asiakkaat = db.Customers.ToList();
-            return Ok(asiakkaat);
+            try
+            {
+                var asiakkaat = db.Customers.ToList();
+                return Ok(asiakkaat);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää:" + ex.InnerException);
+            }
         }
+
+        [HttpGet("{id}")]
+        public ActionResult GetOneCustomersById(String id)
+        {
+            try
+            {
+                var asiakas = db.Customers.Find(id);
+                if (asiakas != null)
+                {
+                    return Ok(asiakas);
+                }
+                else
+                {
+                    //return BadRequest("Asiakasta id:llä " + id + "ei löydy");
+                    return NotFound($"Asiakasta id:llä {id} ei löydy");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + ex);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddNew([FromBody] Customer cust)
+        {
+            try 
+            {
+                db.Customers.Add(cust);
+                db.SaveChanges();
+                return Ok($"Lisättiin uusi asiakas {cust.CompanyName} from {cust.City}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Tapahtui virhe.Lue lisää: " + ex.InnerException);
+            }
+        }
+
     }
 }
